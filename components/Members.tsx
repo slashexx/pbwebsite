befeeb6c62625202cb02f78f21913dc4d7ed1473
2004yash <134356616+2004yash@users.noreply.github.com>
2024-12-20 02:26:37 +0530

    Fix dependency array in useEffect for Members component

--- components/Members.tsx
+++ components/Members.tsx
@@ -74,7 +74,7 @@ export default function Members() {
             }
           }
         });
-      });
+      },[isAdmin]);
 
 
     const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
